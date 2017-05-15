37ba4764af733ad3af7620d8e6d461ed04e14179
Chaz Henricks <chazhenricks@gmail.com>
2017-05-15 15:48:23 -0500

    stashing this stuff so I can check ryans branch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
+//Sidebar toggle function
 console.log("MAIN.JS");
+    $("#menu-toggle").click(function(e) {
+        $("#wrapper").toggleClass("toggled");
+    });
+
+let Handlebars = require("hbsfy/runtime"),
+    mainTemplate = require("../master-template.hbs");
+
+
+
+
+let mainObj = {};
+
+$("#page").append(mainTemplate(mainObj));
